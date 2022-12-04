cbd7049f4e4c52003e38577025742d230fe22b67
Daniel Agg <daniel.agg@outlook.com>
2022-12-04 13:45:21 +0100

    day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,4 +7,9 @@ fun main() {
     println("Day 2:")
     day2.solution().part1()
     day2.solution().part2()
+
+    println("-------------------")
+    println("Day 3:")
+    day3.solution().part1()
+    day3.solution().part2()
 }
---
+++ src/main/kotlin/day3/solution.kt
@@ -0,0 +1,79 @@
+package day3
+
+
+class solution {
+
+    private val inputFilePath = "src/main/kotlin/day3/input.txt"
+
+    fun getValueOfLetter(letter: Char): Int {
+        val alphabet = charArrayOf(
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z'
+        )
+
+        val lowerCaseValue = alphabet.indexOf(letter.lowercaseChar()) + 1
+        val upperCaseValue = lowerCaseValue + 26
+
+        return if(letter.isLowerCase()) lowerCaseValue else upperCaseValue
+    }
+
+    fun part1() { // 7850
+        var result = 0
+
+        Utils.readFile(inputFilePath)
+            .map { it ->
+                val middle = it.length / 2
+                val leftCompartment = it.substring(0, middle).toList()
+                val rightCompartment = it.substring(middle, it.length).toList()
+
+                leftCompartment.intersect(rightCompartment).map {
+                    result += getValueOfLetter(it)
+                }
+            }
+
+        println(result)
+    }
+
+    fun part2() { // 2581
+        var result = 0
+        val lines = Utils.readFile(inputFilePath)
+
+        for (i in 0 until lines.count() step 3)
+        {
+            val group1 = lines[i].toList()
+            val group2 = lines[i + 1].toList()
+            val group3 = lines[i + 2].toList()
+
+            group1.intersect(group2).intersect(group3).map{
+                result += getValueOfLetter(it)
+            }
+        }
+
+        println(result)
+    }
+
+}
